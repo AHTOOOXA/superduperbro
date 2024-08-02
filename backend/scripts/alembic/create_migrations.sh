40309e6b9845ad0c411f926e6f969e8e5a0ef5d8
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-08-02 14:19:42 +0300

    fixed migrations

--- backend/scripts/alembic/create_migrations.sh
+++ backend/scripts/alembic/create_migrations.sh
@@ -1,3 +1,3 @@
 read -p "Enter name of migration: " message
-export BOT_CONTAINER_NAME=medsync_bot
+export BOT_CONTAINER_NAME=telegram-fastapi-vue-bot
 docker exec ${BOT_CONTAINER_NAME} alembic revision --autogenerate -m "$message"
