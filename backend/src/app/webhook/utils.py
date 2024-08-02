3849e8ef6e30ef4963b36df0e289781fba3d21bc
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-08-02 14:36:16 +0300

    black missed

--- backend/src/app/webhook/utils.py
+++ backend/src/app/webhook/utils.py
@@ -33,22 +33,16 @@ def validate_telegram_data(init_data: str) -> bool:
 
     received_hash = parsed_data.pop("hash")
     # Constructing the data-check-string
-    fields = sorted(
-        [(key, unquote(value)) for key, value in parsed_data.items() if key != "hash"]
-    )
+    fields = sorted([(key, unquote(value)) for key, value in parsed_data.items() if key != "hash"])
     # Constructing the data-check-string using the sorted order
     data_check_string = "\n".join(f"{k}={v}" for k, v in fields)
 
     print(data_check_string)
     # Computing the secret key
-    secret_key = hmac.new(
-        b"WebAppData", tgbot_config.token.encode(), hashlib.sha256
-    ).digest()
+    secret_key = hmac.new(b"WebAppData", tgbot_config.token.encode(), hashlib.sha256).digest()
 
     # Comparing received hash with computed hash
-    computed_hash = hmac.new(
-        secret_key, data_check_string.encode(), hashlib.sha256
-    ).hexdigest()
+    computed_hash = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
 
     if computed_hash != received_hash:
         return False
